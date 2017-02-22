44b4ee3b73e4d7ecf527f26d6dd6ae9b508f5ec5
Huy Duong <Huy.Duong@mgm-tp.com>
2017-02-22 23:34:32 +0700

    Change browser's name

--- chrome/common/chrome_constants.cc
+++ chrome/common/chrome_constants.cc
@@ -10,11 +10,11 @@
 #define FPL FILE_PATH_LITERAL
 
 #if defined(OS_MACOSX)
-#define CHROMIUM_PRODUCT_STRING "Chromium"
+#define CHROMIUM_PRODUCT_STRING "Chronos"
 #if defined(GOOGLE_CHROME_BUILD)
 #define PRODUCT_STRING "Google Chrome"
 #elif defined(CHROMIUM_BUILD)
-#define PRODUCT_STRING "Chromium"
+#define PRODUCT_STRING "Chronos"
 #else
 #error Unknown branding
 #endif
